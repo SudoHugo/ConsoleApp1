6f9bdd13bae99ceb003afccd7547b2c303b5de54
SudoHugo <hugo.molin@elev.ga.ntig.se>
2020-01-15 10:02:20 +0100

    plz

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            weff    34f3    4f134f
+        }
+    }
+}
